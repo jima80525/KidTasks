a7da5cd699d1e82f8b91e021953e691376fab8fa
Jim Anderson <jima80525@gmail.com>
2016-09-15 21:44:29 -0600

    Building up models.  Have tasks, countedTasks, DateSchedule, and DayOfWeekSchedule somewhat working

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,43 +1,47 @@
 from django.contrib import admin
 
-from .models import Task, Kid, CountedTask, DayOfWeekSchedule, DateSchedule
-from .models import WeekSchedule, Schedule, HistoricDate, History
+from .models import Task, CountedTask, DayOfWeekSchedule, DateSchedule  #, Kid
+#from .models import WeekSchedule, Schedule, HistoricDate, History
+#from .models import Task, Kid, CountedTask, DayOfWeekSchedule, DateSchedule
+#from .models import WeekSchedule, Schedule, HistoricDate, History
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
     list_display = ('name', 'required')
     fields = [('name', 'required')]
 
-@admin.register(Kid)
-class KidAdmin(admin.ModelAdmin):
-    fields = [('name', 'schedule', 'history')]
-
 @admin.register(CountedTask)
 class CountedTaskAdmin(admin.ModelAdmin):
-    fields = [('task', 'count')]
+    fields = [('name', 'count', 'required')]
 
 @admin.register(DayOfWeekSchedule)
 class DayOfWeekScheduleAdmin(admin.ModelAdmin):
-    list_display = ('day_name',)
-    fields = [('tasks', 'day_name')]
+    filter_horizontal = ('tasks', )
+    list_display = ('name', 'day_name',  )
+    fields = [('name', 'day_name', 'tasks')]
 
 @admin.register(DateSchedule)
 class DateScheduleAdmin(admin.ModelAdmin):
-    fields = [('task', 'date')]
+    filter_horizontal = ('tasks', )
+    fields = [('name', 'tasks', 'date')]
 
-@admin.register(WeekSchedule)
-class WeekScheduleAdmin(admin.ModelAdmin):
-    fields = [('tasks', 'start_date')]
+#@admin.register(WeekSchedule)
+#class WeekScheduleAdmin(admin.ModelAdmin):
+    #fields = [('tasks', 'start_date')]
 
-@admin.register(Schedule)
-class ScheduleAdmin(admin.ModelAdmin):
-    fields = [('name', 'day_tasks', 'date_tasks', 'week_tasks')]
+#@admin.register(Schedule)
+#class ScheduleAdmin(admin.ModelAdmin):
+    #fields = [('name', 'day_tasks', 'date_tasks', 'week_tasks')]
 
-@admin.register(HistoricDate)
-class HistoricDateAdmin(admin.ModelAdmin):
-    fields = [('date', 'tasks', 'counted_tasks')]
+#@admin.register(HistoricDate)
+#class HistoricDateAdmin(admin.ModelAdmin):
+    #fields = [('date', 'tasks', 'counted_tasks')]
 
-@admin.register(History)
-class HistoryDateAdmin(admin.ModelAdmin):
-    fields = [('dates')]
+#@admin.register(History)
+#class HistoryDateAdmin(admin.ModelAdmin):
+    #fields = [('dates')]
+
+#@admin.register(Kid)
+#class KidAdmin(admin.ModelAdmin):
+    #fields = [('name', 'schedule', 'history')]
 
--- tasks/models.py
+++ tasks/models.py
@@ -4,38 +4,77 @@ class Task(models.Model):
     name = models.CharField(max_length=256)
     required = models.BooleanField()
 
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name', ]
+
 class CountedTask(models.Model):
-    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    name = models.CharField(max_length=256)
+    required = models.BooleanField()
     count = models.IntegerField()
 
+    def __str__(self):
+        return "{} ({:d})".format(self.name, self.count)
+
+    class Meta:
+        ordering = ['name', 'count']
+
 class DayOfWeekSchedule(models.Model):
+    day_of_week_choices = (
+        ('M', 'Monday'),
+        ('T', 'Tuesday'),
+        ('W', 'Wednesday'),
+        ('R', 'Thursday'),
+        ('F', 'Friday'),
+        ('S', 'Saturday'),
+        ('N', 'Sunday'),
+    )
+
+    name = models.CharField(max_length=256)
     tasks = models.ManyToManyField(Task)
-    day_name = models.CharField(max_length=256)
+    day_name = models.CharField(max_length=1, choices=day_of_week_choices)
+
+    def __str__(self):
+        full_day = dict(self.day_of_week_choices)[self.day_name]
+        return "{}-{}".format(self.name, full_day)
+
+    class Meta:
+        ordering = ['name', 'day_name']
 
 class DateSchedule(models.Model):
+    """ here"""
+    name = models.CharField(max_length=256)
     tasks = models.ManyToManyField(Task)
     date = models.DateField()
 
-class WeekSchedule(models.Model):
-    tasks = models.ManyToManyField(CountedTask)
-    start_date = models.DateField()
+    def __str__(self):
+        return "{}-{}".format(self.name, self.date)
 
-class Schedule(models.Model):
-    name = models.CharField(max_length=256)
-    day_tasks = models.ManyToManyField(DayOfWeekSchedule)
-    date_tasks = models.ManyToManyField(DateSchedule)
-    week_tasks = models.ManyToManyField(WeekSchedule)
+    class Meta:
+        ordering = ['name', 'date']
 
-class HistoricDate(models.Model):
-    date = models.DateField()
-    tasks = models.ManyToManyField(Task)
-    counted_tasks = models.ManyToManyField(CountedTask)
+#class WeekSchedule(models.Model):
+    #tasks = models.ManyToManyField(CountedTask)
+    #start_date = models.DateField()
 
-class History(models.Model):
-    dates = models.ManyToManyField(HistoricDate)
+#class Schedule(models.Model):
+    #name = models.CharField(max_length=256)
+    #day_tasks = models.ManyToManyField(DayOfWeekSchedule)
+    #date_tasks = models.ManyToManyField(DateSchedule)
+    #week_tasks = models.ManyToManyField(WeekSchedule)
 
-class Kid(models.Model):
-    name = models.CharField(max_length=256)
-    schedule = models.ForeignKey(Schedule, on_delete=models.CASCADE)
-    history = models.ForeignKey(History, on_delete=models.CASCADE)
+#class HistoricDate(models.Model):
+    #date = models.DateField()
+    #tasks = models.ManyToManyField(Task)
+    #counted_tasks = models.ManyToManyField(CountedTask)
+
+#class History(models.Model):
+    #dates = models.ManyToManyField(HistoricDate)
+
+#class Kid(models.Model):
+    #name = models.CharField(max_length=256)
+    #schedule = models.ForeignKey(Schedule, on_delete=models.CASCADE)
+    #history = models.ForeignKey(History, on_delete=models.CASCADE)
 
