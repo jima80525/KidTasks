f9cc8c9a8bf6d706fee8390c2248f7231a88a8f2
Jim Anderson <jima80525@gmail.com>
2017-05-15 21:23:01 -0600

    playing with postgres database

--- KidTasks/settings.py
+++ KidTasks/settings.py
@@ -81,9 +81,16 @@ WSGI_APPLICATION = 'KidTasks.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'kidtasks',
+        'USER': 'jima',
+        'PASSWORD': 'jimajima',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
